da762e8bcc90b7cc5a0ff545e6eef48fd0099c6a
Przemysław Skrzyniarz <przemek.sk@interia.eu>
2021-02-09 13:16:12 +0100

    Adding Application Structure

---
+++ src/ts/App.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+
+const App: FunctionComponent<{}> = (props) => {
+
+  return (
+      <div className="container">
+        <div className="row">
+        </div>
+      </div>
+  );
+};
+
+export default App;
